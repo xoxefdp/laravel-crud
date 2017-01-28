9d0acb1dd3023b79e992430954d0c94da61be44c
José Diaz <xoxefdp@gmail.com>
2017-01-27 21:03:19 -0500

    Removed style classes to insert input form

--- resources/views/form-insert.blade.php
+++ resources/views/form-insert.blade.php
@@ -15,11 +15,11 @@
       <form name="formInsert" action="" method="POST">
         {{ csrf_field() }}
         <td class="id">&nbsp;</td>
-        <td class="nombres"><input class="nombres" type="text" name="nombres" placeholder="NOMBRES" required="required"></td>
-        <td class="apellidos"><input class="apellidos" type="text" name="apellidos" placeholder="APELLIDOS" required="required"></td>
-        <td class="cedula"><input class="cedula" type="number" name="cedula" placeholder="CÉDULA" required="required"></td>
-        <td class="correo"><input class="correo" type="email" name="correo" placeholder="CORREO" required="required"></td>
-        <td class="telefono"><input class="telefono" type="tel" name="telefono" placeholder="TELÉFONO" required="required"></td>
+        <td class="nombres"><input type="text" name="nombres" placeholder="NOMBRES" required="required"></td>
+        <td class="apellidos"><input type="text" name="apellidos" placeholder="APELLIDOS" required="required"></td>
+        <td class="cedula"><input type="number" name="cedula" placeholder="CÉDULA" required="required"></td>
+        <td class="correo"><input type="email" name="correo" placeholder="CORREO" required="required"></td>
+        <td class="telefono"><input type="tel" name="telefono" placeholder="TELÉFONO" required="required"></td>
         <td class="operaciones">
           <div class="btn-group" role="group">
             <button class="btn button" type="reset" name="reset">limpiar</button>
